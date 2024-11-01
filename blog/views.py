316f07cf49abfb919591cfdb1449a2d4ad65f2d0
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-01 20:55:26 -0300

    Exibindo o erro 404 (página não encontrada) com django.http.Http404 (Not Found)

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
+from blog.data import posts
+from typing import Any
+from django.http import HttpRequest, Http404
 # Create your views here.
 def blog(request):
     print('blog')
     context = {
-        'text': 'olá blog'
+        'text': 'olá blog',
+        'posts': posts,
     }
     return render(
             request, 
             'blog/index.html',
+            context
+        )
+def post(request: HttpRequest, post_id : int):
+    found_post: dict[str, Any] | None = None
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+    if found_post is None:
+        raise Http404('post nao existe')
+    context = {
+        # 'text': 'olá blog',
+        'post': found_post,
+        'title': found_post['title'] + ' - '
+    }
+    return render(
+            request, 
+            'blog/post.html',
             context
         )
 def exemplo(request):
